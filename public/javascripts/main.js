a1b633d10111cc240c4f79705afdef8a35ea3b75
Bryan <brykenote@gmail.com>
2017-03-16 18:05:24 +0000

    fix bug: add event listeners to new quote

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,6 +6,10 @@ $(function () {
             'quote': $('#create-quote').val()
         };
         
+        if(!validParams(createData.name, createData.quote)) {
+            return;
+        }
+        
         $.ajax({
             type: 'POST',
             url: '/quotes',
@@ -13,13 +17,13 @@ $(function () {
             headers: {'Content-Type': 'application/json'},
             data: JSON.stringify(createData),
             success: function (data) {
-                var quoteTemplate = $('#quotes > li').first().clone();
+                var quoteTemplate = $('#quotes > li').first().clone(true, true);
                 quoteTemplate.attr('id', data._id);
                 quoteTemplate.find('.name-text').html(data.name);
                 quoteTemplate.find('.quote-text').html(data.quote);
                 $('#quotes').append(quoteTemplate);
             }
-        })
+        });
     });
     
     // delete quote btn
@@ -57,7 +61,6 @@ $(function () {
     $('.quote > .update-info > .update-confirm').on('click', function () {
         var thisElem = $(this);
         var updatedData = {
-            //'id': thisElem.attr('id'),
             'id': thisElem.last().parent().parent().prop('id'),
             'name': thisElem.siblings('input.update-name').val(),
             'quote': thisElem.siblings('input.update-quote').val(),
@@ -77,3 +80,13 @@ $(function () {
         });
     });
 });
+
+function validParams() {
+    var result = true;
+    var args = Array.prototype.slice.call(arguments);
+    args.forEach(function (arg) {
+        if(arg.trim() === '')
+            result = false;
+    });
+    return result;
+}
