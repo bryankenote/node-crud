3f1aced703ad9a6f8af3a7217147ac1ab03f49d9
Bryan <brykenote@gmail.com>
2017-05-27 15:49:08 +0000

    prepare to deploy on heroku

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/CRUDy_quotes');
+//var db = monk('localhost:27017/CRUDy_quotes');
+var db = monk(process.env.MONGOLAB_URI);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
